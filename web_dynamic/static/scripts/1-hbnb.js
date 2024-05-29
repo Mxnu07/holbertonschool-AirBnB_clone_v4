3aec351b7e2da8a131bfd76b0bf28b3c05c714f1
MMD <manuelmoralesdiaz0@gmail.com>
2024-05-29 14:10:50 -0400

    adding files for task 2

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,16 @@
+$(document).ready(() => {
+    const checklist = {};
+    let amen = [];
+    $('INPUT:checkbox').change(function () {
+      if ($(this).is(':checked')) {
+        const key = $(this).attr('data-id');
+        const value = $(this).attr('data-name');
+        checklist[key] = value;
+      }
+      if (!$(this).is(':checked')) {
+        delete checklist[$(this).attr('data-id')];
+      }
+      amen = Object.values(checklist);
+      $('.amenities h4').text(amen.join(', '));
+    });
+  });
